28e7b2b4a1df5b276a89a4a1a67e9dc875dccd0f
Omar Navarro <jomarnavarro@gmail.com>
2021-05-31 10:54:44 -0500

    Converts project to testng and uses a grid infrastructure

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,29 +1,30 @@
 package tests;
 
-import org.junit.Test;
-import pages.HomePage;
-import pages.LoginPage;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class LoginTests extends TestParent {
 
-    @Test
-    public void loginCorrecto() throws Exception {
-        login.logIn("sergio", "Test@1234");
+    @Test(dataProvider = "correctLogin", dataProviderClass = params.LoginParams.class)
+    public void loginCorrecto(String username, String password) throws Exception {
+        login.logIn(username, password);
         home.validateElements();
         menu.validateLoggedInElements();
     }
 
-    @Test
-    public void loginIncorrecto() throws Exception {
-        login.logIn("Pedro", "Pedro");
+    @Test(dataProvider = "incorrectLogin", dataProviderClass = params.LoginParams.class)
+    public void loginIncorrecto(String username, String password) throws Exception {
+        login.logIn(username, password);
         error.validateMessage("invalid username or password");
+        System.out.println(driver.getCurrentUrl());
 
     }
 
-    @Test
-    public void loginSinPassword() throws Exception {
-        login.logIn("sergio", "");
+    @Test(dataProvider = "emptyPasswordLogin", dataProviderClass = params.LoginParams.class)
+    public void loginSinPassword(String username, String emptyPassword) throws Exception {
+        login.logIn(username, emptyPassword);
         error.validateMessage("must provide password");
+        System.out.println(driver.getCurrentUrl());
     }
 
 }
